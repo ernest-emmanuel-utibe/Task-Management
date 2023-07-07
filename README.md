9394ee2f442bf9ec1161cbff41f04cdce265f53a
Ernest <em123nest@gmail.com>
2023-07-07 18:32:43 +0100

    Completed Project

--- README.md
+++ README.md
@@ -1,35 +1,44 @@
-TASK_MANAGEMENT_SYSTEM
+#### TASK_MANAGEMENT_SYSTEM
 
-INTRODUCTION:
+### INTRODUCTION:
 
 Task management system is a web application developed for team project works where users can manage various task within team easily.
 It is developed using Spring Boot(backend) framework.
 
-Project is focused mainly on handling document based projects.
-This web application is very secure and simple. I have implemented OAuth2 based Authentication and Authorization technique in our application.
+The project is focused mainly on handling document based projects.
+This web application is very secure and simple. I have implemented JWT Authentication and Authorization technique in this application.
 The password is encrypted using public and private keys then is stored in database. I am using MySQL and Spring Data JPA for database management.
 
 
-Dependencies
-Java 17 JDK
-Embedded Tomcat 9 server
-MySQL Database
-Maven Installation
-Backend
-In IntelliJ IDE or similar IDE import the "backend" from this repo with option "import existing maven project".
+## Dependencies
+    Spring Web
+    Spring Data JDBC
+    MySQL Database
+    Spring Security
+    Lombok
+    Spring starter Web
+    Java 17 JDK
+    Embedded Tomcat 9 server
+    Maven Installation
+
+## Backend
+
+In IntelliJ IDE or similar IDE import the "backend" from this repo with option "import existing maven project.
+
 Build the maven project to install all the required dependencies.
+
 To set up database, install MySQL. Make any database.
-Then update below three configuration fields in file application.properties inside /resources folder
-spring.datasource.url=jdbc:mysql://localhost:8080/task_management_api_db
-spring.datasource.username=root
-spring.datasource.password=root
-For email services you need to use email via SMTP. For that you need to update application.properties below fields.
-spring.mail.host=smtp.gmail.com
-spring.mail.smtp.ssl.trust=smtp.gmail.com
-spring.mail.port=587
-spring.mail.transport.protocol=smtp
-spring.mail.username=your.email@gmail.com
-spring.mail.password=password`
+
+
+Then update below the configuration fields in file
+# application.properties
+    spring.datasource.url=jdbc:mysql://localhost:3306/task_management_api_db?createDatabaseIfNotExist=true
+    spring.datasource.driverClassName=com.mysql.cj.jdbc.Driver
+    spring.jpa.database-platform=org.hibernate.dialect.MySQLDialect
+    spring.jpa.hibernate.ddl-auto=update
+    server.port=8080
+    spring.datasource.username={use your database username}
+    spring.datasource.password={use your database password}
+
 
 Run the project from TaskManagementApplication.java, all the tables will be initialised in database with its first run.
-Now execute roles.sql on your database.
---
+++ src/test/java/com/api/taskManagement/services/impl/UserServiceImplTest.java
@@ -0,0 +1,113 @@
+//package com.api.taskManagement.services.impl;
+//
+//import com.api.taskManagement.data.dto.request.UserLoginRequest;
+//import com.api.taskManagement.data.dto.request.UserRegistrationRequest;
+//import com.api.taskManagement.data.models.User;
+//import com.api.taskManagement.data.repository.UserRepository;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import org.mockito.junit.MockitoJUnitRunner;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//@RunWith(MockitoJUnitRunner.class)
+//public class UserServiceImplTest {
+//    @Mock
+//    private UserRepository userRepository;
+//
+//    @Mock
+//    private PasswordEncoder passwordEncoder;
+//
+//    @InjectMocks
+//    private UserServiceImpl userService;
+//
+//
+//    @Test
+//    public void registerUser_NewUsername_SuccessfullyRegistersUser() {
+//        // Prepare test data
+//        UserRegistrationRequest request = new UserRegistrationRequest();
+//        request.setUsername("testuser");
+//        request.setPassword("password123");
+//
+//        Mockito.when(userRepository.findByUsername(request.getUsername())).thenReturn(null);
+//
+//        // Execute the method
+//        userService.registerUser(request);
+//
+//        // Verify the method calls
+//        Mockito.verify(userRepository, Mockito.times(1)).save(Mockito.any(User.class));
+//    }
+//
+//    @Test
+//    public void registerUser_ExistingUsername_ThrowsException() {
+//        // Prepare test data
+//        UserRegistrationRequest request = new UserRegistrationRequest();
+//        request.setUsername("testuser");
+//        request.setPassword("password123");
+//
+//        User existingUser = new User();
+//        existingUser.setUsername("testuser");
+//
+//        Mockito.when(userRepository.findByUsername(request.getUsername())).thenReturn(existingUser);
+//
+//        // Execute and assert
+//        assertThrows(RuntimeException.class, () -> userService.registerUser(request));
+//    }
+//
+//    @Test
+//    public void loginUser_ValidCredentials_ReturnsJwtToken() {
+//        // Prepare test data
+//        UserLoginRequest request = new UserLoginRequest();
+//        request.setUsername("testuser");
+//        request.setPassword("password123");
+//
+//        User user = new User();
+//        user.setUsername("testuser");
+//        user.setPassword("$2a$10$R91u..y..9vMA7E65Y2PKOelHfUnFr.GN6bpYOMqG2TRGnlTfYyc.");
+//
+//        Mockito.when(userRepository.findByUsername(request.getUsername())).thenReturn(user);
+//        Mockito.when(passwordEncoder.matches(request.getPassword(), user.getPassword())).thenReturn(true);
+//
+//        // Execute the method
+//        String jwtToken = userService.loginUser(request);
+//
+//        // Assert the result
+//        assertEquals("JWT_TOKEN", jwtToken);
+//    }
+//
+//    @Test
+//    public void loginUser_InvalidUsername_ThrowsException() {
+//        // Prepare test data
+//        UserLoginRequest request = new UserLoginRequest();
+//        request.setUsername("testuser");
+//        request.setPassword("password123");
+//
+//        Mockito.when(userRepository.findByUsername(request.getUsername())).thenReturn(null);
+//
+//        // Execute and assert
+//        assertThrows(RuntimeException.class, () -> userService.loginUser(request));
+//    }
+//
+//    @Test
+//    public void loginUser_InvalidPassword_ThrowsException() {
+//        // Prepare test data
+//        UserLoginRequest request = new UserLoginRequest();
+//        request.setUsername("testuser");
+//        request.setPassword("password123");
+//
+//        User user = new User();
+//        user.setUsername("testuser");
+//        user.setPassword("$2a$10$R91u..y..9vMA7E65Y2PKOelHfUnFr.GN6bpYOMqG2TRGnlTfYyc.");
+//
+//        Mockito.when(userRepository.findByUsername(request.getUsername())).thenReturn(user);
+//        Mockito.when(passwordEncoder.matches(request.getPassword(), user.getPassword())).thenReturn(false);
+//
+//        // Execute and assert
+//        assertThrows(RuntimeException.class, () -> userService.loginUser(request));
+//    }
+//}
