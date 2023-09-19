5cde2ae2d45c0ee0e85adef80996f3a8f03ebca2
Ernest Emmanuel Utibe <em123nest@gmail.com>
2023-09-19 13:05:27 +0100

    Update UserServiceImplTest.java

--- src/test/java/com/api/taskManagement/services/impl/UserServiceImplTest.java
+++ src/test/java/com/api/taskManagement/services/impl/UserServiceImplTest.java
@@ -23,23 +23,6 @@
 //    @Mock
 //    private PasswordEncoder passwordEncoder;
 //
-//    @Test
-//    public void loginUser_ValidCredentials_ReturnsJwtToken() {
-//        // Prepare test data
-//        UserLoginRequest request = new UserLoginRequest();
-//        request.setUsername("testuser");
-//        request.setPassword("password123");
-//
-//        User user = new User();
-//        user.setUsername("testuser");
-//        user.setPassword("$2a$10$R91u..y..9vMA7E65Y2PKOelHfUnFr.GN6bpYOMqG2TRGnlTfYyc.");
-//
-//        Mockito.when(userRepository.findByUsername(request.getUsername())).thenReturn(user);
-//        Mockito.when(passwordEncoder.matches(request.getPassword(), user.getPassword())).thenReturn(true);
-//
-//        // Execute the method
-//        String jwtToken = userService.loginUser(request);
-//
 //        // Assert the result
 //        assertEquals("JWT_TOKEN", jwtToken);
 //    }
